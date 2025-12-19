803461f05b854d4882f254a181bda23375c57911
funckyou0818-cpu <funckyou0818@gmail.com>
2025-12-19 11:43:14 +0900

    update

--- deploy-scripts/start.sh
+++ deploy-scripts/start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-APP_DIR=/home/ec2-user/app
-JAR_NAME=$(ls $APP_DIR/*.jar)
+APP_DIR=/home/ec2-user/app/build/libs
+LOG_DIR=/home/ec2-user/logs
 
-cd $APP_DIR
+mkdir -p $LOG_DIR
 
-nohup java -jar $JAR_NAME > app.log 2>&1 &
+JAR_NAME=$(ls $APP_DIR/*SNAPSHOT.jar | grep -v plain | head -n 1)
+
+echo "Starting application: $JAR_NAME"
+
+nohup java -jar $JAR_NAME > $LOG_DIR/app.log 2>&1 &
