2e896dac2b2812e4f7ed678aa484abd8eaa471d3
funckyou0818-cpu <funckyou0818@gmail.com>
2025-12-19 12:16:36 +0900

    update

--- deploy-scripts/start.sh
+++ deploy-scripts/start.sh
@@ -10,3 +10,6 @@ JAR_NAME=$(ls $APP_DIR/*SNAPSHOT.jar | grep -v plain | head -n 1)
 echo "Starting application: $JAR_NAME"
 
 nohup java -jar $JAR_NAME > $LOG_DIR/app.log 2>&1 &
+
+sleep 10
+exit 0
--- deploy-scripts/stop.sh
+++ deploy-scripts/stop.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-pkill -f 'java -jar'
+
+PID=$(pgrep -f 'aivle_4th_MiniProject')
+
+if [ -z "$PID" ]; then
+  echo "No application process found. Skipping stop."
+  exit 0
+fi
+
+echo "Stopping application with PID: $PID"
+kill -15 $PID
+sleep 5
+
+exit 0
