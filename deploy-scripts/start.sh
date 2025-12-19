966e5c9ec5e6a6972b66972c41b23e035fbb0f62
funckyou0818-cpu <funckyou0818@gmail.com>
2025-12-19 17:12:27 +0900

    up

--- deploy-scripts/start.sh
+++ deploy-scripts/start.sh
@@ -4,15 +4,10 @@ APP_DIR=/home/ec2-user/app/build/libs
 LOG_DIR=/home/ec2-user/logs
 
 mkdir -p $LOG_DIR
-chown -R ec2-user:ec2-user $LOG_DIR
 
 JAR_NAME=$(ls $APP_DIR/*SNAPSHOT.jar | grep -v plain | head -n 1)
 
-if [ -z "$JAR_NAME" ]; then
-  echo "❌ JAR not found in $APP_DIR"
-  exit 1
-fi
-
 nohup java -jar $JAR_NAME > $LOG_DIR/app.log 2>&1 &
 
+sleep 20
 exit 0
